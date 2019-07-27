0a138659363526904712835ce1b788c64a8a695a
yenhuang1116 <yenhuang1116@gmail.com>
2019-07-28 02:13:41 +0800

    correct file
    correct file

--- int2str/main.c
+++ int2str/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-char* int2str(const int* input,const int input_size);
+
+char* int2str(const int input);
 void _main();
 
 int main()
@@ -13,21 +13,12 @@ int main()
 
 void _main()
 {
-    int a[2] = {23,25};
-    char* ret = int2str(a,2);
+    char* ret = int2str(226);
     printf("result = %s",ret);
 }
-char* int2str(const int* input,const int input_size)
+char* int2str(const int input)
 {
-    char ret[20] = {'\0'};
-    strcpy(ret,"2323");
-    puts(ret);
-    int i;
-    for(i=0;i<20;i++)
-    {
-        if(ret[i]=='\0')puts("NULL");
-    }
-
-    //sprintf(ret, "%d%d", input[0], input[1]);
+    char ret[20];
+    sprintf(ret, "%d", input);
     return ret;
 }
